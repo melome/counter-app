ad0bdf4bcf4fba1ab5c8d972f31bb64ddf00a7bf
Shanley Grace Rosero <shanleyrosero@gmail.com>
2022-07-05 05:26:11 +0800

    Initial commit; react using class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    count: 0
+  }
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+
+  render(){
+    return (
+      <div style={{textAlign: 'center'}}>
+        <h2>counter app</h2>
+        <button onClick={this.increment}>Increment</button>
+        <br />
+        {this.state.count > 0 ? 
+        <p>You clicked {this.state.count} times</p> 
+        :<p>Click the button!</p>}
+      </div>
+    )
+  }
 }
 
 export default App;
