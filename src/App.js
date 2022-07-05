c32d5b907d26ae3272f9695debfa3760edb18b63
Shanley Grace Rosero <shanleyrosero@gmail.com>
2022-07-05 09:52:10 +0800

    Made changes to make use of useState Hook

--- src/App.js
+++ src/App.js
@@ -1,29 +1,48 @@
-import React, {Component} from 'react'
+import React, {Component, useState} from 'react'
 
-class App extends Component {
+const App = () => {
+  const [count, setCount] = useState(0);
 
-  state = {
-    count: 0
+  const increment = () => {
+    setCount(count + 1);
   }
 
-  increment = () => {
-    this.setState({
-      count: this.state.count + 1
-    });
-  }
-
-  render(){
-    return (
-      <div style={{textAlign: 'center'}}>
-        <h2>counter app</h2>
-        <button onClick={this.increment}>Increment</button>
-        <br />
-        {this.state.count > 0 ? 
-        <p>You clicked {this.state.count} times</p> 
-        :<p>Click the button!</p>}
-      </div>
-    )
-  }
+  return (
+    <div style={{textAlign: 'center'}}>
+      <h2>counter app</h2>
+      <button onClick={increment}>Increment</button>
+      <br />
+      {count > 0 ? 
+      <p>You clicked {count} times</p> 
+      :<p>Click the button!</p>}
+    </div>
+  );
 }
+
+// class App extends Component {
+
+//   state = {
+//     count: 0
+//   }
+
+//   increment = () => {
+//     this.setState({
+//       count: this.state.count + 1
+//     });
+//   }
+
+//   render(){
+//     return (
+//       <div style={{textAlign: 'center'}}>
+//         <h2>counter app</h2>
+//         <button onClick={this.increment}>Increment</button>
+//         <br />
+//         {this.state.count > 0 ? 
+//         <p>You clicked {this.state.count} times</p> 
+//         :<p>Click the button!</p>}
+//       </div>
+//     )
+//   }
+// }
 
 export default App;
